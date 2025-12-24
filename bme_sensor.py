7cf4c32e2aa76dde3071742703d0fb1e2d951dff
kenkanekiken <kennethlim619@icloud.com>
2025-12-24 22:31:38 +0800

    Completed BME280 logic and relay fan logic

---
+++ bme_sensor.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as G
+import bme280
+import smbus2
+import threading
+
+port = 1
+address = 0x76
+bus = smbus2.SMBus(port)
+
+calibration_params = bme280.load_calibration_params(bus, address)
+
+def bmeRead():
+    global data
+    data = bme280.sample(bus, address, calibration_params)
+    print(f"Temp: {data.temperature:.2f}  Hum: {data.humidity:.2f}  Pres: {data.pressure:.2f}")
+    threading.Timer(2,bmeRead).start()
+    
