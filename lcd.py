304e633f0043fade7d73083f4485d85a8478922f
kenkanekiken <kennethlim619@icloud.com>
2026-02-09 10:22:23 +0800

    Last Commit

--- lcd.py
+++ lcd.py
@@ -4,8 +4,6 @@ import time
 lcd = LCD()
 
 def lcdReading(temp, hum):
-#     lcd.text(f"Temp: {temp:.2f}", 1)
-#     lcd.text(f"Hum: {hum:.2f}", 2)
     safe_text(f"Temp: {temp:.2f}", 1)
     safe_text(f"Hum: {hum:.2f}", 2)
 
@@ -14,7 +12,6 @@ def safe_text(msg, line):
         lcd.text(str(msg)[:16], line)   # trim to 16 chars
     except OSError as e:
         print("LCD I2C error:", e)
-        # optional small cooldown so bus can recover
         time.sleep(0.05)
 
 def clear_safe():
